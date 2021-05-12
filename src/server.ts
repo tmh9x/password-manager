577fcac590be34e42cdb7f464761dd2213dc82b5
tmh9x <thomas.hermanu@gmail.com>
2021-05-12 10:34:53 +0200

    :sparkles: Implement list service functionality

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
-import { askForMainPassword } from './utils/questions';
+import { printPassword } from './utils/messages';
+import {
+  askForMainPassword,
+  chooseCommand,
+  chooseService,
+} from './utils/questions';
 import { isMainPasswordValid } from './utils/validation';
 
 //   let mainPassword = await askForMainPassword();
@@ -15,6 +20,20 @@ const start = async () => {
     start();
   } else {
     console.log('Is valid');
+
+    const command = await chooseCommand();
+
+    switch (command) {
+      case 'list':
+        {
+          const service = await chooseService(['GitHub', 'Codewars', 'Google']);
+          printPassword(service);
+        }
+        break;
+      case 'add':
+        console.log('Add Case');
+        break;
+    }
   }
 };
 
--- src/utils/questions.ts
+++ src/utils/questions.ts
@@ -1,4 +1,5 @@
 import inquirer from 'inquirer';
+import { Command } from '../types';
 
 // export function askForMainPassword(); Promise<string> {
 export const askForMainPassword = (): Promise<string> => {
@@ -11,4 +12,27 @@ export const askForMainPassword = (): Promise<string> => {
       },
     ])
     .then((answers) => answers.mainPassword);
+};
+
+export const chooseCommand = async (): Promise<Command> => {
+  const answers = await inquirer.prompt<{ command: Command }>({
+    type: 'list',
+    name: 'command',
+    message: 'What do you wand to do?',
+    choices: [
+      { name: 'List all credentials', value: 'list' },
+      { name: 'Add new credential', value: 'add' },
+    ],
+  });
+  return answers.command;
+};
+
+export const chooseService = async (services: string[]): Promise<string> => {
+  const answers = await inquirer.prompt<{ service: string }>({
+    type: 'list',
+    name: 'service',
+    message: 'Please choose a service',
+    choices: services,
+  });
+  return answers.service;
 };

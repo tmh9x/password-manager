b0b743ce4a34f0b6d2d704d06c2fab37779c9242
tmh9x <thomas.hermanu@gmail.com>
2021-05-19 11:30:17 +0200

    :heavy_plus_sign: Add express server

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const port = 5000;
+
+app.get('/api/credentials', (_request, response) => {
+  response.send('All credentials requested');
+});
+
+app.post('/api/credentials', (_request, response) => {
+  response.send('Add new credential');
+});
+
+app.listen(port, () => {
+  console.log(`password-manager listening at http://localhost:${port}`);
+});

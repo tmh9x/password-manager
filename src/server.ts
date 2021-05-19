210f32be4bde6589e2ce688e6e495cb16a82a56a
tmh9x <thomas.hermanu@gmail.com>
2021-05-19 14:10:03 +0200

    :sparkles: Add express server & add more samples

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,29 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
+import { readCredentials } from './utils/credentials';
+import { connectDatabase } from './utils/database';
+
+if (process.env.MONGO_URL === undefined) {
+  throw new Error('Missing env MONGO_URL');
+}
 
 const app = express();
 const port = 5000;
 
-app.get('/api/credentials', (_request, response) => {
-  response.send('All credentials requested');
+app.get('/api/credentials', async (_request, response) => {
+  const credentials = await readCredentials();
+  response.json(credentials);
 });
 
 app.post('/api/credentials', (_request, response) => {
   response.send('Add new credential');
 });
 
-app.listen(port, () => {
-  console.log(`password-manager listening at http://localhost:${port}`);
+connectDatabase(process.env.MONGO_URL).then(() => {
+  console.log('Database connected');
+  app.listen(port, () => {
+    console.log(`Cryptarch listening at http://localhost:${port}`);
+  });
 });
